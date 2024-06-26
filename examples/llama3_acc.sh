db24e0eca2a27a7a2c1c8c48111e64923e2b631b
wenting.swt <wenting.swt@alibaba-inc.com>
2024-06-26 20:09:07 +0800

    Support llama3 (#2)

---
+++ examples/llama3_acc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+
+# FSDP
+# note: this need transformers>=4.41.0
+./examples/run.sh --model ./hf_models/config/llama-3-1b --accelerator acc --gc --mbs 2 --fsdp 8 --max_seq_length 4096 --no_fa
