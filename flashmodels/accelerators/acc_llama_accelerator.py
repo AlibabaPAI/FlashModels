97861c23a016bf12d360b511093d6434e96e3774
luzhan <luzhan.lz@alibaba-inc.com>
2024-11-01 16:28:24 +0800

    fix: enable low cpu mem usage for fsdpv2 + ulysses

--- flashmodels/accelerators/acc_llama_accelerator.py
+++ flashmodels/accelerators/acc_llama_accelerator.py
@@ -79,8 +79,8 @@ class ACCLLAMAAccelerator(Accelerator):
         model.model.config.use_cache = False
 
         if self.args.sp_num > 1 and self.args.spmd_fsdp:
-            device = lazy_device()
-            model.to(device)
+            # device = lazy_device()
+            # model.to(device)
             model = self.ulysses(model)
             # return model, loader
 
@@ -208,9 +208,15 @@ class ACCLLAMAAccelerator(Accelerator):
             output = m._original_forward(*args, **kwargs)
             return output
 
-        gc_cnt = self.args.gc_cnt
+        device = lazy_device()
         model.lm_head.forward = MethodType(_forward_linear, model.lm_head)
         for decoder_layer in model.model.layers:
+            is_torchdistX_deferred_init = (LOW_CPU_MEM_USAGE and any(
+                fake.is_fake(param) for param in decoder_layer.parameters()))
+            if is_torchdistX_deferred_init:
+                deferred_init.materialize_module(decoder_layer)
+            decoder_layer.to(device)
+
             if hasattr(decoder_layer.self_attn, "_create_sp_mesh"):
                 decoder_layer.self_attn._create_sp_mesh(self.args.sp_num)
 
@@ -238,7 +244,14 @@ class ACCLLAMAAccelerator(Accelerator):
             #     if gc_cnt > 0:
             #         decoder_layer = checkpoint_module(decoder_layer)
             #         gc_cnt -= 1
-
+        is_torchdistX_deferred_init = (LOW_CPU_MEM_USAGE and any(
+            fake.is_fake(param) for param in model.parameters()))
+        if is_torchdistX_deferred_init:
+            deferred_init.materialize_module(
+                model,
+                check_fn=lambda k: not isinstance(k, type(model.model.layers[0]
+                                                          )))
+        model.to(device)
         return model
 
     def parallel_3d(self, model):
@@ -387,8 +400,10 @@ class ACCLLAMAAccelerator(Accelerator):
             is_torchdistX_deferred_init = (LOW_CPU_MEM_USAGE and any(
                 fake.is_fake(param) for param in decoder_layer.parameters()))
             if is_torchdistX_deferred_init:
+                print("materialize module")
                 deferred_init.materialize_module(decoder_layer)
             decoder_layer.to(device)
+            print(f"after materialize decoder layer fake: {any(fake.is_fake(param) for param in decoder_layer.parameters())}")
             # attn
             if hasattr(decoder_layer.self_attn, "_create_tp_mesh"):
                 decoder_layer.self_attn._create_tp_mesh(
